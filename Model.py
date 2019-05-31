d2c61a5e823bddc478801135884904a6b91944c2
BraulioSanchez <braulio2791@gmail.com>
2019-05-30 21:54:10 -0300

    minor fixes

--- Model.py
+++ Model.py
@@ -15,7 +15,7 @@ class Model():
             return_sequences = layer['return_sequences'] if 'return_sequences' in layer else None
             output_shape = layer['output_shape'] if 'output_shape' in layer else None
 
-            if layer['type'] == 'dense':
+            if layer['type'] == 'lstm':
                 self.model.add(LSTM(units=neurons,
                                     activation=activation,
                                     input_shape=(input_timesteps, input_dim),
@@ -24,14 +24,17 @@ class Model():
                 self.model.add(RepeatVector(output_shape))
             if layer['type'] == 'time_distributed':
                 if layer['layer']['type'] == 'dense':
-                    neurons = layer['layer']['neurons'] if 'neurons' in layer else None
-                    activation = layer['layer']['activation'] if 'activation' in layer else None
+                    neurons = layer['layer']['neurons'] if 'neurons' in layer['layer'] else None
+                    activation = layer['layer']['activation'] if 'activation' in layer['layer'] else None
                     self.model.add(TimeDistributed(Dense(units=neurons, activation=activation)))
 
 
 if __name__ == "__main__":
     import json
     configs = json.load(open('config.json', 'r'))
-
+    
     model = Model()
     model.build(configs)
+
+    from keras.utils import plot_model
+    plot_model(model.model, show_shapes=True, to_file="lstm-model-1.png")
